55fa623180b61f9e722cccffd3446992d3b9d9a7
David Welles <74557848+davidwelles@users.noreply.github.com>
2021-03-15 18:35:05 -0400

    commit assignment2

---
+++ assign2.js
@@ -0,0 +1,24 @@
+//Problem one solution:
+// async function getRandomNumber() {
+//     let promise= new Promise(resolve => {
+//           setTimeout(() => {
+//             resolve(Math.random());
+//           }, 500);
+//         });
+//    let result= await promise;
+//    alert(result);
+// }
+      
+// getRandomNumber();
+
+//Problem 2 solution:
+async function city(cityName) {
+  // create a variable that goes ahead and fetches the link  (sub value indicated through ${})
+  const response = await fetch(`https://geocode.xyz/${cityName}?json=1`)
+  // create a variable that awaits its conversion to a json file
+  const info = await response.json();
+  //print out a line that has text and coords
+  console.log("\nThe latitude is: " + info.longt + ".\n\nThe longitude is: " + info.latt);
+}
+//input a city!
+city("charlotte");
